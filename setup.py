75f7cee8551a8b8d74fee951f7bcd3dfaaac5c9e
Mohamadhossein <mhamadhosseinsharifi13@gmail.com>
2022-04-24 06:52:03 +0430

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fixer-demo',
+    version='0.1',
+    description='Fixer service demo package',
+    url='#',
+    author='Zeus',
+    author_email='mhamadhosseinsharifi13@gmail.com',
+    license='MIT',
+    packages=['fixer'],
+    zip_safe=False)
